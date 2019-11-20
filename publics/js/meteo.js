182d4ffab38585b2f3287825285a108fd5a1eb4e
fra9106 <55781079+fra9106@users.noreply.github.com>
2019-11-20 13:30:58 +0100

    application météo js commentaires + réindent + nettoyage code

--- publics/js/meteo.js
+++ publics/js/meteo.js
@@ -1,63 +1,61 @@
- const weatherIcon = {
-         "Rain": "wi wi-day-rain",
-         "Clouds": "wi wi-day-cloudy",
-         "Clear": "wi wi-day-sunny",
-         "Snow": "wi wi-day-snow",
-         "Mist": "wi wi-day-fog",
-         "Drizzle": "wi wi-day-sleet",
-         "Clouds-high": "wi-day-cloudy-high",
-      }
+const weatherIcon = { // objet liste d'icon
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-sunny",
+    "Snow": "wi wi-day-snow",
+    "Mist": "wi wi-day-fog",
+    "Drizzle": "wi wi-day-sleet",
+}
 
-      function capitalize(str){
-         return str[0].toUpperCase() + str.slice(1);
-      }
+function capitalize(str) { // capitalise la première lettre du texte
+    return str[0].toUpperCase() + str.slice(1); //et on rajoute le reste du tableau à partir de la 2ème lettre
+}
 
-                async function main(withIp = true){ // adresse ip de l'utilisateur
+async function main(withIp = true) { // fonction par défaut: on affiche la ville de l'user avec son ip
 
-                let ville;
+    let ville;
 
-               if(withIp){
-                 ville = await fetch('http://api.ipstack.com/ 176.159.91.31?access_key=91913480e438d4f8cb02499b9389bfe7')
-                 .then(resultat => resultat.json())
-                 .then(json => json.city)
-                 console.log(ville);
-              }else{
-                ville = document.querySelector('#ville').textContent;
-             }
-             const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=2e0e5ede742861192d75ef2771bd649e&lang=fr&units=metric`)
+    if (withIp) { // recherche de son ip pour afficher la ville user: API IPSTACK
+        ville = await fetch('http://api.ipstack.com/ 176.159.91.31?access_key=91913480e438d4f8cb02499b9389bfe7')
+            .then(resultat => resultat.json()) //quand tu as le resultat tu me le donne en json...
+            .then(json => json.city) //...et quand tu as le json, va me chercher la ville (city dans json)
+        console.log(ville); // tu m'affiches la ville dans la console
+    } else { // on passe la fonction par défaut et on peut récupérer la ville par le texte tapé dans l'id ville
+        ville = document.querySelector('#ville').textContent;
+    }
+    //récupération de la variable ville dans l'addresse de l'API OPENWEATHERMAP
+    const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=2e0e5ede742861192d75ef2771bd649e&lang=fr&units=metric`)
 
-             .then(resultat => resultat.json())
-             .then(json => json)
-             console.log(meteo);
-             displayWeatherInfos(meteo)
-          }
+        .then(resultat => resultat.json())
+        .then(json => json)
+    console.log(meteo);
+    displayWeatherInfos(meteo)
+}
 
-          function displayWeatherInfos(data){
-            const name = data.name;
-            const temperature = data.main.temp;
-            const conditions = data.weather[0].main;
-            const description = data.weather[0].description;
+function displayWeatherInfos(data) { // appel des différents items de l'objet...
+    const name = data.name;
+    const temperature = data.main.temp;
+    const conditions = data.weather[0].main;
+    const description = data.weather[0].description;
 
-            document.querySelector('#ville').textContent =name;
-            document.querySelector('#temperature').textContent = Math.round(temperature);
-            document.querySelector('#conditions').textContent = capitalize(description);
+    document.querySelector('#ville').textContent = name; // ...pour pouvoir les afficher
+    document.querySelector('#temperature').textContent = Math.round(temperature); // arrondi le chiffre de température
+    document.querySelector('#conditions').textContent = capitalize(description); // capitalise la première lettre
+    // affiche l'icon selon les conditions grâce à l'ojbet weatherIcon situé en haut du fichier
+    document.querySelector('i.wi').className = weatherIcon[conditions];
+}
+//initialisation au click du contentEditable pour pouvoir taper le nom de la ville
+ville.addEventListener('click', () => {
+    ville.contentEditable = true;
+});
+//pour ne pas sauter de ligne au click de la touche entrée
+ville.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        ville.contentEditable = false;
+        // pour ne plus avoir la fonction main par défaut et qu'on puisse taper la ville que l'on veut    
+        main(false);
+    }
+})
 
-            document.querySelector('i.wi').className = weatherIcon[conditions];
-            document.body.className = conditions.toLowerCase();
-         }
-
-         const result = document.querySelector('#ville');
-
-         ville.addEventListener('click', () => {
-          ville.contentEditable = true;
-       });
-
-         ville.addEventListener('keydown', (e) =>{
-            if(e.keyCode === 13){
-               e.preventDefault();
-               ville.contentEditable = false;
-               main(false);
-            }
-         })
-
-         main();
+main();
