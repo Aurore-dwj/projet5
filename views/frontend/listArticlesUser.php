f091534f96282a72a89f05d671107f128459a684
fra9106 <55781079+fra9106@users.noreply.github.com>
2019-11-21 12:34:42 +0100

    pagination articles user par catégories ok!

--- views/frontend/listArticlesUser.php
+++ views/frontend/listArticlesUser.php
@@ -67,19 +67,19 @@ $data = $artic;
 $artic->closeCursor(); 
  ?>
 
-<nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
+<div align="center">
+<?php
+
+      for($i=1;$i<=$totalpages;$i++) {
+         if($i == $pageCourante) {
+          echo $i.'&nbsp;&nbsp;';
+         } else {
+            echo '<a href="index.php?action=listArticlesUser&amp;id_rubrique='.$_GET['id_rubrique'].'&amp;page='.$i.'"> '.$i.'</a>&nbsp;&nbsp;';
+         }
+      }
+      ?>
+
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
